661d1930a01fa188622ec26098c863074cba1e9e
Karl Hiner <karl.hiner@gmail.com>
2021-12-18 20:55:22 -0800

    Outline `app_reducer` function

--- src/reducer.h
+++ src/reducer.h
@@ -1,5 +1,12 @@
 #pragma once
 
-struct Reducer {
+#include "state.h"
 
-};
+State app_reducer(State state, action) {
+    switch (action) {
+        default:
+            // If this reducer doesn't recognize the action type, or doesn't
+            // care about this specific action, return the existing state unchanged
+            return state
+    }
+}

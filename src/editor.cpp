59c9e426fd76da379d2f6a86542e4077edebd6c6
Karl Hiner <karl.hiner@gmail.com>
2022-03-24 19:19:11 -0700

    Clean up zep editor code since it was copied directly and edited little from https://github.com/cmaughan/zep_imgui

--- src/editor.cpp
+++ src/editor.cpp
@@ -4,49 +4,25 @@
 
 #include "editor.h"
 #include "config.h"
-#include <functional>
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
 using namespace Zep;
 
-using cmdFunc = std::function<void(const std::vector<std::string> &)>;
-
-class ZepCmd : public ZepExCommand {
-public:
-    ZepCmd(ZepEditor &editor, const std::string name, cmdFunc fn)
-        : ZepExCommand(editor), m_name(name), m_func(fn) {
-    }
-
-    virtual void Run(const std::vector<std::string> &args) override {
-        m_func(args);
-    }
-
-    virtual const char *ExCommandName() const override {
-        return m_name.c_str();
-    }
-
-private:
-    std::string m_name;
-    cmdFunc m_func;
-};
-
 struct ZepWrapper : public Zep::IZepComponent {
     ZepWrapper(const fs::path &root_path, const Zep::NVec2f &pixelScale, std::function<void(std::shared_ptr<Zep::ZepMessage>)> fnCommandCB)
-        : zepEditor(Zep::ZepPath(root_path.string()), pixelScale), Callback(fnCommandCB) {
+        : zepEditor(Zep::ZepPath(root_path.string()), pixelScale), Callback(std::move(fnCommandCB)) {
         zepEditor.RegisterCallback(this);
 
     }
 
-    virtual Zep::ZepEditor &GetEditor() const override {
+    Zep::ZepEditor &GetEditor() const override {
         return (Zep::ZepEditor &) zepEditor;
     }
 
-    virtual void Notify(std::shared_ptr<Zep::ZepMessage> message) override {
+    void Notify(std::shared_ptr<Zep::ZepMessage> message) override {
         Callback(message);
-
-        return;
     }
 
     virtual void HandleInput() {
@@ -59,10 +35,13 @@ struct ZepWrapper : public Zep::IZepComponent {
 
 std::shared_ptr<ZepWrapper> spZep;
 
+// Initialize the editor and watch for changes
 void zep_init(const Zep::NVec2f &pixelScale) {
-    // Initialize the editor and watch for changes
-    spZep = std::make_shared<ZepWrapper>(config.app_root, Zep::NVec2f(pixelScale.x, pixelScale.y), [](std::shared_ptr<ZepMessage> spMessage) -> void {
-    });
+    spZep = std::make_shared<ZepWrapper>(
+        config.app_root,
+        Zep::NVec2f(pixelScale.x, pixelScale.y),
+        [](const std::shared_ptr<ZepMessage> &_) -> void {}
+    );
 
     auto &display = spZep->GetEditor().GetDisplay();
     auto pImFont = ImGui::GetIO().Fonts[0].Fonts[0];
@@ -75,21 +54,15 @@ void zep_init(const Zep::NVec2f &pixelScale) {
 }
 
 void zep_update() {
-    if (spZep) {
-        spZep->GetEditor().RefreshRequired();
-    }
+    if (spZep) spZep->GetEditor().RefreshRequired();
 }
 
 void zep_destroy() {
     spZep.reset();
 }
 
-ZepEditor &zep_get_editor() {
-    return spZep->GetEditor();
-}
-
 void zep_load(const Zep::ZepPath &file) {
-    auto pBuffer = zep_get_editor().InitWithFileOrDir(file);
+    auto pBuffer = spZep->GetEditor().InitWithFileOrDir(file);
 }
 
 void zep_show(const Zep::NVec2i &displaySize) {
@@ -102,10 +75,8 @@ void zep_show(const Zep::NVec2i &displaySize) {
 
     auto min = ImGui::GetCursorScreenPos();
     auto max = ImGui::GetContentRegionAvail();
-    if (max.x <= 0)
-        max.x = 1;
-    if (max.y <= 0)
-        max.y = 1;
+    if (max.x <= 0) max.x = 1;
+    if (max.y <= 0) max.y = 1;
     ImGui::InvisibleButton("ZepContainer", max);
 
     // Fill the window
@@ -114,15 +85,7 @@ void zep_show(const Zep::NVec2i &displaySize) {
 
     spZep->zepEditor.SetDisplayRegion(Zep::NVec2f(min.x, min.y), Zep::NVec2f(max.x, max.y));
     spZep->zepEditor.Display();
-    bool zep_focused = ImGui::IsWindowFocused();
-    if (zep_focused) {
-        spZep->zepEditor.HandleInput();
-    }
+    if (ImGui::IsWindowFocused()) spZep->zepEditor.HandleInput();
 
-    // TODO: A Better solution for this; I think the audio graph is creating a new window and stealing focus
-    static int focus_count = 0;
-    if (focus_count++ < 2) {
-        ImGui::SetWindowFocus();
-    }
     ImGui::End();
 }
--- src/editor.h
+++ src/editor.h
@@ -2,8 +2,6 @@
 
 #include <zep.h>
 
-// Helpers to create zep editor
-Zep::ZepEditor &zep_get_editor();
 void zep_init(const Zep::NVec2f &pixelScale);
 void zep_update();
 void zep_show(const Zep::NVec2i &displaySize);

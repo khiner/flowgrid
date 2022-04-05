2bdbc01a1802d9cea5af9a19175821bb71b8cbf2
Karl Hiner <karl.hiner@gmail.com>
2022-04-05 08:24:08 -0700

    More cleanup in `faust_editor.cpp`

--- src/window/windows/faust_editor.cpp
+++ src/window/windows/faust_editor.cpp
@@ -13,19 +13,17 @@ using namespace Zep;
 namespace fs = std::filesystem;
 
 struct ZepWrapper : public Zep::IZepComponent {
-    ZepWrapper(const fs::path &root_path, const Zep::NVec2f &pixelScale, std::function<void(std::shared_ptr<Zep::ZepMessage>)> fnCommandCB)
-        : editor(Zep::ZepPath(root_path.string()), pixelScale), Callback(std::move(fnCommandCB)) {
+    ZepWrapper(const fs::path &root_path, const Zep::NVec2f &pixel_scale, std::function<void(std::shared_ptr<Zep::ZepMessage>)> callback)
+        : editor(Zep::ZepPath(root_path.string()), pixel_scale), callback(std::move(callback)) {
         editor.RegisterCallback(this);
     }
 
     Zep::ZepEditor &GetEditor() const override { return (Zep::ZepEditor &) editor; }
 
-    void Notify(std::shared_ptr<Zep::ZepMessage> message) override { Callback(message); }
-
-    virtual void HandleInput() { editor.HandleInput(); }
+    void Notify(std::shared_ptr<Zep::ZepMessage> message) override { callback(message); }
 
     Zep::ZepEditor_ImGui editor;
-    std::function<void(std::shared_ptr<Zep::ZepMessage>)> Callback;
+    std::function<void(std::shared_ptr<Zep::ZepMessage>)> callback;
 };
 
 std::unique_ptr<ZepWrapper> zep;
@@ -35,7 +33,9 @@ void zep_init() {
     zep = std::make_unique<ZepWrapper>(
         config.app_root,
         Zep::NVec2f(pixelScale.x, pixelScale.y),
-        [](const std::shared_ptr<ZepMessage> &) -> void {}
+        [](const std::shared_ptr<ZepMessage> &) -> void {
+//            std::cout << message->str << std::endl;
+        }
     );
 
     auto &display = zep->editor.GetDisplay();
@@ -74,39 +74,24 @@ void zep_draw() {
 // Simple text editor
 struct InputTextCallback_UserData {
     std::string *Str;
-    ImGuiInputTextCallback ChainCallback;
-    void *ChainCallbackUserData;
 };
 
 static int InputTextCallback(ImGuiInputTextCallbackData *data) {
     auto *user_data = (InputTextCallback_UserData *) data->UserData;
     if (data->EventFlag == ImGuiInputTextFlags_CallbackResize) {
-        // Resize string callback
-        // If for some reason we refuse the new length (BufTextLen) and/or capacity (BufSize) we need to set them back to what we want.
         std::string *str = user_data->Str;
-        IM_ASSERT(data->Buf == str->c_str());
         str->resize(data->BufTextLen);
         data->Buf = (char *) str->c_str();
-    } else if (user_data->ChainCallback) {
-        // Forward to user callback, if any
-        data->UserData = user_data->ChainCallbackUserData;
-        return user_data->ChainCallback(data);
     }
     return 0;
 }
 
-bool InputTextMultiline(const char *label, std::string *str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = nullptr, void *user_data = nullptr) {
-    IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
-    flags |= ImGuiInputTextFlags_CallbackResize;
-
-    InputTextCallback_UserData cb_user_data{str, callback, user_data};
-    return ImGui::InputTextMultiline(label, (char *) str->c_str(), str->capacity() + 1, ImVec2(0, 0), flags, InputTextCallback, &cb_user_data);
-}
-
 void simple_draw() {
 //        ImGuiInputTextFlags_NoUndoRedo;
-    static auto flags = ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_EnterReturnsTrue;
-    if (InputTextMultiline("##faust_source", &ui_s.audio.faust.code, flags)) {
+    static auto flags = ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackResize;
+    std::string *str = &ui_s.audio.faust.code;
+    InputTextCallback_UserData cb_user_data{str};
+    if (ImGui::InputTextMultiline("##faust_source", (char *) str->c_str(), str->capacity() + 1, ImVec2(0, 0), flags, InputTextCallback, &cb_user_data)) {
         q.enqueue(set_faust_text{ui_s.audio.faust.code});
     }
     ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 0, 0, 255));

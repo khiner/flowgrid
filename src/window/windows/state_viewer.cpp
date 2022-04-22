6e5e513dcce2461273501cd2440dc886a188bdfe
Karl Hiner <karl.hiner@gmail.com>
2022-04-22 15:38:21 -0700

    Highlight text for annotated labels in state viewer

--- src/window/windows/state_viewer.cpp
+++ src/window/windows/state_viewer.cpp
@@ -2,25 +2,36 @@
 #include "../../context.h"
 #include "../../imgui_helpers.h"
 
-static void add_json_state_value_node(const std::string &key, const json &value, bool annotate = false) {
+using Settings = WindowsBase::StateViewerWindow::Settings;
+
+bool HighlightedTreeNode(const char *label, bool is_highlighted = false) {
+    if (is_highlighted) ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 255, 0, 255)); // TODO register a highlight color in style
+    bool is_open = ImGui::TreeNode(label);
+    if (is_highlighted) ImGui::PopStyleColor();
+
+    return is_open;
+}
+
+static void add_json_state_value_node(const std::string &key, const json &value, bool is_annotated_key = false) {
+    bool annotate = s.ui.windows.state_viewer.settings.label_mode == Settings::annotated;
     //      ImGuiTreeNodeFlags_DefaultOpen or SetNextItemOpen()
     if (value.is_null()) {
         ImGui::Text("null");
     } else if (value.is_object()) {
-        if (ImGui::TreeNode(key.c_str())) {
+        if (HighlightedTreeNode(key.c_str(), is_annotated_key)) {
             for (auto it = value.begin(); it != value.end(); ++it) {
-                add_json_state_value_node(it.key(), it.value(), annotate);
+                add_json_state_value_node(it.key(), it.value());
             }
             ImGui::TreePop();
         }
     } else if (value.is_array()) {
         bool annotate_color = annotate && key == "Colors";
-        if (ImGui::TreeNode(key.c_str())) {
+        if (HighlightedTreeNode(key.c_str(), is_annotated_key)) {
             int i = 0;
             for (const auto &it: value) {
-                const auto &name = annotate_color && i < ImGuiCol_COUNT ? ImGui::GetStyleColorName(i) : std::to_string(i);
-                // TODO color annotated labels
-                add_json_state_value_node(name, it, annotate);
+                const bool is_child_annotated_key = annotate_color && i < ImGuiCol_COUNT;
+                const auto &name = is_child_annotated_key ? ImGui::GetStyleColorName(i) : std::to_string(i);
+                add_json_state_value_node(name, it, is_child_annotated_key);
                 i++;
             }
             ImGui::TreePop();
@@ -32,7 +43,7 @@ static void add_json_state_value_node(const std::string &key, const json &value,
 
 static const std::string label_help = "The raw JSON state doesn't store keys for all items.\n"
                                       "For example, the main `ui.style.colors` state is a list.\n\n"
-                                      "'Annotated' mode shows labels for such state items.\n"
+                                      "'Annotated' mode shows (highlighted) labels for such state items.\n"
                                       "'Raw' mode shows the state exactly as it is in the raw JSON state.";
 
 void StateViewer::draw(Window &) {
@@ -40,10 +51,10 @@ void StateViewer::draw(Window &) {
         if (ImGui::BeginMenu("Settings")) {
             if (BeginMenuWithHelp("Label mode", label_help.c_str())) {
                 auto label_mode = s.ui.windows.state_viewer.settings.label_mode;
-                if (ImGui::MenuItem("Annotated", nullptr, label_mode == WindowsBase::StateViewerWindow::Settings::annotated)) {
-                    q.enqueue(set_state_viewer_label_mode{Windows::StateViewerWindow::Settings::LabelMode::annotated});
-                } else if (ImGui::MenuItem("Raw", nullptr, label_mode == WindowsBase::StateViewerWindow::Settings::raw)) {
-                    q.enqueue(set_state_viewer_label_mode{Windows::StateViewerWindow::Settings::LabelMode::raw});
+                if (ImGui::MenuItem("Annotated", nullptr, label_mode == Settings::annotated)) {
+                    q.enqueue(set_state_viewer_label_mode{Settings::LabelMode::annotated});
+                } else if (ImGui::MenuItem("Raw", nullptr, label_mode == Settings::raw)) {
+                    q.enqueue(set_state_viewer_label_mode{Settings::LabelMode::raw});
                 }
                 ImGui::EndMenu();
             }
@@ -52,6 +63,5 @@ void StateViewer::draw(Window &) {
         ImGui::EndMenuBar();
     }
 
-    bool annotate = s.ui.windows.state_viewer.settings.label_mode == WindowsBase::StateViewerWindow::Settings::annotated;
-    add_json_state_value_node("State", c.json_state, annotate);
+    add_json_state_value_node("State", c.json_state);
 }

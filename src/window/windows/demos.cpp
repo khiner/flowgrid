8444340bc2691853fef5b7c8be9acd7c30b4aaae
Karl Hiner <karl.hiner@gmail.com>
2022-05-03 00:28:23 -0700

    Split up `imgui_windows.cpp` into `metrics.cpp` and `demos.cpp`

---
+++ src/window/windows/demos.cpp
@@ -0,0 +1,18 @@
+#include "../../state.h"
+
+#include "imgui.h"
+#include "implot.h"
+
+void Windows::Demos::draw() {
+    if (ImGui::BeginTabBar("##tabs")) {
+        if (ImGui::BeginTabItem("ImGui")) {
+            ImGui::ShowDemo();
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("ImPlot")) {
+            ImPlot::ShowDemo();
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
+    }
+}
--- src/window/windows/metrics.cpp
+++ src/window/windows/metrics.cpp
@@ -1,21 +1,6 @@
 #include "../../state.h"
 
 #include "imgui.h"
-#include "implot.h"
-
-void Windows::Demos::draw() {
-    if (ImGui::BeginTabBar("##tabs")) {
-        if (ImGui::BeginTabItem("ImGui")) {
-            ImGui::ShowDemo();
-            ImGui::EndTabItem();
-        }
-        if (ImGui::BeginTabItem("ImPlot")) {
-            ImPlot::ShowDemo();
-            ImGui::EndTabItem();
-        }
-        ImGui::EndTabBar();
-    }
-}
 
 void Windows::Metrics::draw() {
     if (ImGui::BeginTabBar("##tabs")) {

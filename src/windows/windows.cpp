95028833c708aedfd2ba6de724a64b646647cf80
Karl Hiner <karl.hiner@gmail.com>
2022-05-12 18:49:40 -0700

    Remove now-unused `wrap_draw_in_window` option in `draw_window`

--- src/windows/windows.cpp
+++ src/windows/windows.cpp
@@ -1,23 +1,19 @@
+#include "../stateful_imgui.h"
 #include "../context.h"
 
-// TODO move `wrap_draw_in_window` into a new `StatefulImGuiWindowFlags : ImGuiWindowFlags` type
-void draw_window(Window &window, ImGuiWindowFlags flags = ImGuiWindowFlags_None, bool wrap_draw_in_window = true) {
+void draw_window(Window &window, ImGuiWindowFlags flags = ImGuiWindowFlags_None) {
     const auto &name = window.name;
     if (s.windows.named(name).visible != window.visible) q(toggle_window{name});
     if (!window.visible) return;
 
-    if (wrap_draw_in_window) {
-        if (!ImGui::Begin(name.c_str(), &window.visible, flags)) {
-            ImGui::End();
-            return;
-        }
-    } else {
-        if (!window.visible) return;
+    if (!ImGui::Begin(name.c_str(), &window.visible, flags)) {
+        ImGui::End();
+        return;
     }
 
     window.draw();
 
-    if (wrap_draw_in_window) ImGui::End();
+    ImGui::End();
 }
 
 void Windows::draw() {

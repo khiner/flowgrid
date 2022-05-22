1441414f8b9dc7b590328e6adfe3197c4c3005f4
Karl Hiner <karl.hiner@gmail.com>
2022-05-22 10:40:14 -0700

    Update comment with link to my ImGui GH issue

--- src/state.h
+++ src/state.h
@@ -157,7 +157,7 @@ struct Style : Window {
         name = "Style";
         // Transparent background. Need this to draw in background draw list, behind ImGui contents.
         // Specifically, using this for background rects in tree nodes. Can't find a better way...
-        // TODO post an ImGui issue asking about a way to draw a rect _between_ the window bg color and the ImGui frame content
+        // TODO waiting to hear back on https://github.com/ocornut/imgui/issues/5334
         imgui.Colors[ImGuiCol_WindowBg].w = 0;
     }
 

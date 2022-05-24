fb2013ede7e187b20aa068b5dc855040f7d8bafa
Karl Hiner <karl.hiner@gmail.com>
2022-05-24 16:41:06 -0700

    Add a `FlowGrid::Metrics` view (along with ImGui/ImPlot metrics), with only a single item so far: action variant size

--- src/windows/metrics.cpp
+++ src/windows/metrics.cpp
@@ -1,7 +1,23 @@
-#include "../state.h"
+#include "../action.h"
+#include "../imgui_helpers.h"
+
+namespace FlowGrid {
+
+void ShowMetrics() {
+    ImGui::Text("Action variant size: %lu bytes", sizeof(Action));
+    ImGui::SameLine();
+    HelpMarker("All actions are internally stored in an `std::variant`, which must be large enough to hold its largest type.\n"
+               "Thus, it's important to keep action data small.");
+}
+
+}
 
 void State::Metrics::draw() {
     if (ImGui::BeginTabBar("##tabs")) {
+        if (ImGui::BeginTabItem("FlowGrid")) {
+            FlowGrid::ShowMetrics();
+            ImGui::EndTabItem();
+        }
         if (ImGui::BeginTabItem("ImGui")) {
             ImGui::ShowMetrics();
             ImGui::EndTabItem();

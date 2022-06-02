248d0e6a33d872b57362d4b1e7a6bf0625dd8de5
Karl Hiner <karl.hiner@gmail.com>
2022-06-01 21:49:51 -0700

    Start of action refactor: replace visitor returning string literals with  static `_name` members shared across all actions

--- src/action.cpp
+++ src/action.cpp
@@ -1,32 +1,28 @@
 #include "action.h"
 
+string undo::_name = "Undo";
+string redo::_name = "Redo";
+string open_project::_name = "Open project";
+string open_empty_project::_name = "Open empty project";
+string open_default_project::_name = "Open default project";
+string save_project::_name = "Save project";
+string save_default_project::_name = "Save default project";
+string save_current_project::_name = "Save current project";
+string close_application::_name = "Close application";
+string set_imgui_settings::_name = "Set ImGui Settings";
+string set_imgui_style::_name = "Set ImGui Style";
+string set_implot_style::_name = "Set ImPlot Style";
+string set_flowgrid_style::_name = "Set FlowGrid Style";
+string toggle_window::_name = "Toggle Window";
+string toggle_state_viewer_auto_select::_name = "Toggle state viewer auto-select";
+string set_state_viewer_label_mode::_name = "Set state-viewer label-mode";
+string toggle_audio_muted::_name = "Toggle audio muted";
+string set_audio_sample_rate::_name = "Set audio sample rate";
+string set_faust_code::_name = "Set faust code";
+string set_audio_running::_name = "Set audio running";
+string toggle_audio_running::_name = "Toggle audio running";
+string set_ui_running::_name = "Set UI running";
+
 string get_action_name(const Action &action) {
-    return std::visit(visitor{
-        [&](const set_imgui_settings &) { return "Set ImGui Settings"; },
-        [&](const set_imgui_style &) { return "Set ImGui Style"; },
-        [&](const set_implot_style &) { return "Set ImPlot Style"; },
-        [&](const set_flowgrid_style &) { return "Set FlowGrid Style"; },
-
-        [&](const toggle_window &) { return "Toggle Window"; },
-
-        [&](const toggle_state_viewer_auto_select &) { return "Toggle state viewer auto-select"; },
-        [&](const set_state_viewer_label_mode &) { return "Set state-viewer label-mode"; },
-        [&](const toggle_audio_muted &) { return "Toggle audio muted"; },
-        [&](const set_audio_sample_rate &) { return "Set audio sample rate"; },
-        [&](const set_faust_code &) { return "Set faust code"; },
-
-        [&](const set_audio_running &) { return "Set audio running"; },
-        [&](const toggle_audio_running &) { return "Toggle audio running"; },
-        [&](const set_ui_running &) { return "Set UI running"; },
-
-        [&](const undo &) { return "Undo"; },
-        [&](const redo &) { return "Redo"; },
-        [&](const open_project &) { return "Open project"; },
-        [&](const open_empty_project &) { return "Open empty project"; },
-        [&](const open_default_project &) { return "Open default project"; },
-        [&](const save_project &) { return "Save project"; },
-        [&](const save_default_project &) { return "Save default project"; },
-        [&](const save_current_project &) { return "Save current project"; },
-        [&](const close_application &) { return "Close application"; },
-    }, action);
+    return std::visit([&](auto &&a) { return a._name; }, action);
 }
--- src/action.h
+++ src/action.h
@@ -16,49 +16,61 @@ struct visitor : Ts ... {
 template<class... Ts> visitor(Ts...)->visitor<Ts...>;
 
 
-// An `Action` is an immutable representation of a user interaction event.
-// Each action stores all information needed for `update` to apply it to a given `State` instance.
+/**
+ * An `Action` is an immutable representation of a user interaction event.
+ * Each action stores all information needed for `update` to apply it to a given `State` instance.
+ *
+ * Static members are prefixed with an underscore to avoid name collisions with data members.
+ * All such static members are declared _after_ data members, to allow for default construction using only non-static members.
+ * Note that adding static members does not increase the size of the `Action` variant of which the `action::` structs are types.
+ * (You can verify this by looking at the 'Action variant size' in the FlowGrid metrics window.)
+*/
 
 namespace action {
+
+struct undo { static string _name; };
+struct redo { static string _name; };
+struct open_project { string path; static string _name; };
+struct save_project { string path; static string _name; };
+struct save_current_project { static string _name; };
+struct open_empty_project { static string _name; };
+struct open_default_project { static string _name; };
+struct save_default_project { static string _name; };
+struct close_application { static string _name; };
 
 // JSON types are used for actions that hold very large structured data.
 // This is because the `Action` `std::variant` below can hold any action type, and variants must be large enough to hold their largest type.
 // As of 5/24/2022, the largest raw action member type was `ImGuiStyle`, which resulted in an `Action` variant size of 1088 bytes.
 // That's pretty silly for a type that can also hold a single boolean value! Replacing with JSON types brought the size down to 32 bytes.
-struct set_imgui_settings { json settings; }; // ImGuiSettings
-struct set_imgui_style { json imgui_style; }; // ImGuiStyle
-struct set_implot_style { json implot_style; }; // ImPlotStyle
-struct set_flowgrid_style { json flowgrid_style; }; // FlowGridStyle
+struct set_imgui_settings { json settings; static string _name; }; // ImGuiSettings
+struct set_imgui_style { json imgui_style; static string _name; }; // ImGuiStyle
+struct set_implot_style { json implot_style; static string _name; }; // ImPlotStyle
+struct set_flowgrid_style { json flowgrid_style; static string _name; }; // FlowGridStyle
 
-struct toggle_window { string name; };
+struct toggle_window { string name; static string _name; };
 
-struct toggle_state_viewer_auto_select {};
-struct set_state_viewer_label_mode { State::StateWindows::StateViewer::LabelMode label_mode; };
+struct toggle_state_viewer_auto_select { static string _name; };
+struct set_state_viewer_label_mode { State::StateWindows::StateViewer::LabelMode label_mode; static string _name; };
 
-struct toggle_audio_muted {};
-struct set_audio_sample_rate { int sample_rate; };
+struct toggle_audio_muted { static string _name; };
+struct set_audio_sample_rate { int sample_rate; static string _name; };
 
-struct set_audio_running { bool running; };
-struct toggle_audio_running {};
-struct set_ui_running { bool running; };
+struct set_audio_running { bool running; static string _name; };
+struct toggle_audio_running { static string _name; };
+struct set_ui_running { bool running; static string _name; };
 
-struct set_faust_code { string text; };
-
-struct undo {};
-struct redo {};
-struct open_project { string path; };
-struct save_project { string path; };
-struct save_current_project {};
-struct open_empty_project {};
-struct open_default_project {};
-struct save_default_project {};
-struct close_application {};
+struct set_faust_code { string text; static string _name; };
 
 }
 
 using namespace action;
 
 using Action = std::variant<
+    undo, redo,
+    open_project, open_empty_project, open_default_project,
+    save_project, save_default_project, save_current_project,
+    close_application,
+
     set_imgui_settings,
     set_imgui_style, set_implot_style, set_flowgrid_style,
 
@@ -73,12 +85,7 @@ using Action = std::variant<
 
     set_audio_running,
     toggle_audio_running,
-    set_ui_running,
-
-    undo, redo,
-    open_project, open_empty_project, open_default_project,
-    save_project, save_default_project, save_current_project,
-    close_application
+    set_ui_running
 >;
 
-string get_action_name(const Action &action);
+string get_action_name(const Action &);

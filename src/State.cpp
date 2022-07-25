a62c1c8cc215a31a4a15672a91b304c2ccb1072a
Karl Hiner <karl.hiner@gmail.com>
2022-07-25 11:56:57 -0400

    Close the file dialog before saving project. (Otherwise, the project opens with the save-file dialog showing.)

--- src/State.cpp
+++ src/State.cpp
@@ -785,33 +785,31 @@ static auto *file_dialog = ImGuiFileDialog::Instance();
 static const string file_dialog_key = "FileDialog";
 
 void File::Dialog::draw() const {
-    if (visible) {
-        // `OpenDialog` is a no-op if it's already open, so it's safe to call every frame.
-        file_dialog->OpenDialog(file_dialog_key, title, filters.c_str(), file_path, default_file_name, max_num_selections, nullptr, flags);
+    if (!visible) return file_dialog->Close();
 
-        // TODO need to get custom vecs with math going
-        const ImVec2 min_dialog_size = {GetMainViewport()->Size.x / 2.0f, GetMainViewport()->Size.y / 2.0f};
-        if (file_dialog->Display(file_dialog_key, ImGuiWindowFlags_NoCollapse, min_dialog_size)) {
-            if (file_dialog->IsOk()) {
-                const fs::path &file_path = file_dialog->GetFilePathName();
-                const string &extension = file_path.extension();
-                if (AllProjectExtensions.find(extension) != AllProjectExtensions.end()) {
-                    // TODO provide an option to save with undo state.
-                    //   This file format would be a json list of diffs.
-                    //   The file would generally be larger, and the load time would be slower,
-                    //   but it would provide the option to save/load _exactly_ as if you'd never quit at all,
-                    //   with full undo/redo history/position/etc.!
-                    if (save_mode) q(save_project{file_path});
-                    else q(open_project{file_path});
-                } else if (extension == FaustDspFileExtension) {
-                    if (save_mode) q(save_faust_file{file_path});
-                    else q(open_faust_file{file_path});
-                }
+    // `OpenDialog` is a no-op if it's already open, so it's safe to call every frame.
+    file_dialog->OpenDialog(file_dialog_key, title, filters.c_str(), file_path, default_file_name, max_num_selections, nullptr, flags);
+
+    // TODO need to get custom vecs with math going
+    const ImVec2 min_dialog_size = {GetMainViewport()->Size.x / 2.0f, GetMainViewport()->Size.y / 2.0f};
+    if (file_dialog->Display(file_dialog_key, ImGuiWindowFlags_NoCollapse, min_dialog_size)) {
+        q(close_file_dialog{}, true);
+        if (file_dialog->IsOk()) {
+            const fs::path &file_path = file_dialog->GetFilePathName();
+            const string &extension = file_path.extension();
+            if (AllProjectExtensions.find(extension) != AllProjectExtensions.end()) {
+                // TODO provide an option to save with undo state.
+                //   This file format would be a json list of diffs.
+                //   The file would generally be larger, and the load time would be slower,
+                //   but it would provide the option to save/load _exactly_ as if you'd never quit at all,
+                //   with full undo/redo history/position/etc.!
+                if (save_mode) q(save_project{file_path});
+                else q(open_project{file_path});
+            } else if (extension == FaustDspFileExtension) {
+                if (save_mode) q(save_faust_file{file_path});
+                else q(open_faust_file{file_path});
             }
-            q(close_file_dialog{});
         }
-    } else {
-        file_dialog->Close();
     }
 }
 

9aae1144af376e61dd941513c80db627efe683cf
Karl Hiner <karl.hiner@gmail.com>
2022-12-19 19:52:33 -0500

    Move action stuff back out into its own header/impl file. Remove a few unused fns.

---
+++ src/FlowGrid/Actions.cpp
@@ -0,0 +1,123 @@
+#include "Actions.h"
+
+#include <range/v3/view/concat.hpp>
+
+using namespace action;
+
+PatchOps Merge(const PatchOps &a, const PatchOps &b) {
+    PatchOps merged = a;
+    for (const auto &[path, op] : b) {
+        if (merged.contains(path)) {
+            const auto &old_op = merged.at(path);
+            // Strictly, two consecutive patches that both add or both remove the same key should throw an exception,
+            // but I'm being lax here to allow for merging multiple patches by only looking at neighbors.
+            // For example, if the first patch removes a path, and the second one adds the same path,
+            // we can't know from only looking at the pair whether the added value was the same as it was before the remove
+            // (in which case it should just be `Remove` during merge) or if it was different (in which case the merged action should be a `Replace`).
+            if (old_op.Op == AddOp) {
+                if (op.Op == RemoveOp || ((op.Op == AddOp || op.Op == ReplaceOp) && old_op.Value == op.Value)) merged.erase(path); // Cancel out
+                else merged[path] = {AddOp, op.Value, {}};
+            } else if (old_op.Op == RemoveOp) {
+                if (op.Op == AddOp || op.Op == ReplaceOp) {
+                    if (old_op.Value == op.Value) merged.erase(path); // Cancel out
+                    else merged[path] = {ReplaceOp, op.Value, old_op.Old};
+                } else {
+                    merged[path] = {RemoveOp, {}, old_op.Old};
+                }
+            } else if (old_op.Op == ReplaceOp) {
+                if (op.Op == AddOp || op.Op == ReplaceOp) merged[path] = {ReplaceOp, op.Value, old_op.Old};
+                else merged[path] = {RemoveOp, {}, old_op.Old};
+            }
+        } else {
+            merged[path] = op;
+        }
+    }
+
+    return merged;
+}
+
+/**
+ Provided actions are assumed to be chronologically consecutive.
+
+ Cases:
+ * `b` can be merged into `a`: return the merged action
+ * `b` cancels out `a` (e.g. two consecutive boolean toggles on the same value): return `true`
+ * `b` cannot be merged into `a`: return `false`
+
+ Only handling cases where merges can be determined from two consecutive actions.
+ One could imagine cases where an idempotent cycle could be determined only from > 2 actions.
+ For example, incrementing modulo N would require N consecutive increments to determine that they could all be cancelled out.
+*/
+std::variant<StateAction, bool> Merge(const StateAction &a, const StateAction &b) {
+    const ID a_id = GetId(a);
+    const ID b_id = GetId(b);
+
+    switch (a_id) {
+        case id<OpenFileDialog>:
+        case id<CloseFileDialog>:
+        case id<ShowOpenProjectDialog>:
+        case id<ShowSaveProjectDialog>:
+        case id<CloseApplication>:
+        case id<SetImGuiColorStyle>:
+        case id<SetImPlotColorStyle>:
+        case id<SetFlowGridColorStyle>:
+        case id<SetDiagramColorStyle>:
+        case id<SetDiagramLayoutStyle>:
+        case id<ShowOpenFaustFileDialog>:
+        case id<ShowSaveFaustFileDialog>: {
+            if (a_id == b_id) return b;
+            return false;
+        }
+        case id<OpenFaustFile>:
+        case id<SetValue>: {
+            if (a_id == b_id && std::get<SetValue>(a).path == std::get<SetValue>(b).path) return b;
+            return false;
+        }
+        case id<SetValues>: {
+            if (a_id == b_id) return SetValues{views::concat(std::get<SetValues>(a).values, std::get<SetValues>(b).values) | to<std::vector>};
+            return false;
+        }
+        case id<ToggleValue>: return a_id == b_id && std::get<ToggleValue>(a).path == std::get<ToggleValue>(b).path;
+        case id<ApplyPatch>: {
+            if (a_id == b_id) {
+                const auto &_a = std::get<ApplyPatch>(a);
+                const auto &_b = std::get<ApplyPatch>(b);
+                // Keep patch actions affecting different base state-paths separate,
+                // since actions affecting different state bases are likely semantically different.
+                const auto &ops = Merge(_a.patch.Ops, _b.patch.Ops);
+                if (ops.empty()) return true;
+                if (_a.patch.BasePath == _b.patch.BasePath) return ApplyPatch{ops, _b.patch.BasePath};
+                return false;
+            }
+            return false;
+        }
+        default: return false;
+    }
+}
+
+Gesture action::MergeGesture(const Gesture &gesture) {
+    Gesture merged_gesture; // Mutable return value
+    // `active` keeps track of which action we're merging into.
+    // It's either an action in `gesture` or the result of merging 2+ of its consecutive members.
+    std::optional<const StateActionMoment> active;
+    for (Count i = 0; i < gesture.size(); i++) {
+        if (!active) active.emplace(gesture[i]);
+        const auto &a = *active;
+        const auto &b = gesture[i + 1];
+        std::variant<StateAction, bool> merge_result = Merge(a.first, b.first);
+        Match(
+            merge_result,
+            [&](const bool cancel_out) {
+                if (cancel_out) i++; // The two actions (`a` and `b`) cancel out, so we add neither. (Skip over `b` entirely.)
+                else merged_gesture.emplace_back(a); //
+                active.reset(); // No merge in either case. Move on to try compressing the next action.
+            },
+            [&](const StateAction &merged_action) {
+                active.emplace(merged_action, b.second); // The two actions were merged. Keep track of it but don't add it yet - maybe we can merge more actions into it.
+            },
+        );
+    }
+    if (active) merged_gesture.emplace_back(*active);
+
+    return merged_gesture;
+}
--- src/FlowGrid/App.h
+++ src/FlowGrid/App.h
@@ -8,7 +8,6 @@
  */
 #include <iostream>
 #include <list>
-#include <map>
 #include <queue>
 #include <set>
 
@@ -18,10 +17,8 @@
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/map.hpp>
 
-#include "Helper/File.h"
+#include "Actions.h"
 #include "Helper/Sample.h"
-#include "Helper/String.h"
-#include "Primitive.h"
 #include "UI/Style.h"
 #include "UI/UI.h"
 
@@ -35,280 +32,20 @@ using namespace StringHelper;
 using fmt::format, fmt::to_string;
 using namespace nlohmann;
 using std::cout, std::cerr;
-using std::map, std::set, std::pair;
+using std::set;
 using std::min, std::max;
-using std::nullopt;
-using std::unique_ptr, std::make_unique;
-
-/**
-An ID is used to uniquely identify something.
-
-## Notable uses
-
-### `StateMember`
-
-A `StateMember` has an `ID id` instance member.
-`StateMember::Id` reflects its `StatePath Path`, using `ImHashStr` to calculate its own `Id` using its parent's `Id` as a seed.
-In the same way, each segment in `StateMember::Path` is calculated by appending its own `PathSegment` to its parent's `Path`.
-This exactly reflects the way ImGui calculates its window/tab/dockspace/etc. ID calculation.
-A drawable `UIStateMember` uses its `ID` (which is also an `ImGuiID`) as the ID for the top-level `ImGui` widget rendered during its `Draw` call.
-This results in the nice property that we can find any `UIStateMember` instance by calling `StateMember::WithId.contains(ImGui::GetHoveredID())` any time during a `UIStateMember::Draw`.
- */
-using ID = ImGuiID;
-using StatePath = fs::path;
-
-static const StatePath RootPath{"/"};
-
-using StoreEntry = pair<StatePath, Primitive>;
-using StoreEntries = vector<StoreEntry>;
-
-struct StatePathHash {
-    auto operator()(const StatePath &p) const noexcept { return fs::hash_value(p); }
-};
-
+using std::nullopt, std::unique_ptr, std::make_unique;
 using Store = immer::map<StatePath, Primitive, StatePathHash>;
 using TransientStore = immer::map_transient<StatePath, Primitive, StatePathHash>;
 
 extern const Store &AppStore; // Read-only global for full, read-only canonical application state.
 
-struct PatchOp {
-    enum Type {
-        Add,
-        Remove,
-        Replace,
-    };
-
-    PatchOp::Type Op{};
-    std::optional<Primitive> Value{}; // Present for add/replace
-    std::optional<Primitive> Old{}; // Present for remove/replace
-};
-
-using PatchOps = map<StatePath, PatchOp>;
-
-static constexpr auto AddOp = PatchOp::Type::Add;
-static constexpr auto RemoveOp = PatchOp::Type::Remove;
-static constexpr auto ReplaceOp = PatchOp::Type::Replace;
-
-struct Patch {
-    PatchOps Ops;
-    StatePath BasePath{RootPath};
-
-    bool empty() const noexcept { return Ops.empty(); }
-};
-
-struct StatePatch {
-    Patch Patch{};
-    TimePoint Time{};
-};
-
-string to_string(const Primitive &);
-string to_string(PatchOp::Type);
-
-//-----------------------------------------------------------------------------
-// [SECTION] Actions
-//-----------------------------------------------------------------------------
-
-/**
-An `Action` is an immutable representation of a user interaction event.
-Each action stores all information needed to apply the action to a `Store` instance.
-An `ActionMoment` contains an `Action` and the `TimePoint` at which the action happened.
-
-An `Action` is a `std::variant`, which can hold any type, and thus must be large enough to hold its largest type.
-- For actions holding very large structured data, using a JSON string is a good approach to keep the `Action` size down
-  (at the expense of losing type safety and storing the string contents in heap memory).
-- Note that adding static members does not increase the size of the parent `Action` variant.
-  (You can verify this by looking at the 'Action variant size' in the Metrics->FlowGrid window.)
-*/
-
-// Utility to make a variant visitor out of lambdas, using the "overloaded pattern" described
-// [here](https://en.cppreference.com/w/cpp/utility/variant/visit).
-template<class... Ts> struct visitor : Ts... {
-    using Ts::operator()...;
-};
-template<class... Ts> visitor(Ts...) -> visitor<Ts...>;
-
-// E.g. Match(action, [](const ProjectAction &a) { ... }, [](const StateAction &a) { ... });
-#define Match(Variant, ...) std::visit(visitor{__VA_ARGS__}, Variant);
-
-// Utility to flatten two variants together into one variant.
-// From https://stackoverflow.com/a/59251342/780425
-// E.g. Combine<Variant1, Variant2>
-template<typename Var1, typename Var2> struct Combine;
-template<typename... Ts1, typename... Ts2> struct Combine<std::variant<Ts1...>, std::variant<Ts2...>> {
-    using type = std::variant<Ts1..., Ts2...>;
-};
-
-namespace Actions {
-struct Undo {};
-struct Redo {};
-struct SetHistoryIndex {
-    int index;
-};
-
-struct OpenProject {
-    string path;
-};
-struct OpenEmptyProject {};
-struct OpenDefaultProject {};
-
-struct ShowOpenProjectDialog {};
-struct OpenFileDialog {
-    string dialog_json;
-}; // Storing as JSON string instead of the raw struct to reduce variant size. (Raw struct is 120 bytes.)
-struct CloseFileDialog {};
-
-struct SaveProject {
-    string path;
-};
-struct SaveCurrentProject {};
-struct SaveDefaultProject {};
-struct ShowSaveProjectDialog {};
-
-struct CloseApplication {};
-
-struct SetValue {
-    StatePath path;
-    Primitive value;
-};
-struct SetValues {
-    StoreEntries values;
-};
-struct ToggleValue {
-    StatePath path;
-};
-struct ApplyPatch {
-    Patch patch;
-};
-
-struct SetImGuiColorStyle {
-    int id;
-};
-struct SetImPlotColorStyle {
-    int id;
-};
-struct SetFlowGridColorStyle {
-    int id;
-};
-struct SetDiagramColorStyle {
-    int id;
-};
-struct SetDiagramLayoutStyle {
-    int id;
-};
-
-struct ShowOpenFaustFileDialog {};
-struct ShowSaveFaustFileDialog {};
-struct ShowSaveFaustSvgFileDialog {};
-struct SaveFaustFile {
-    string path;
-};
-struct OpenFaustFile {
-    string path;
-};
-struct SaveFaustSvgFile {
-    string path;
-};
-} // namespace Actions
-
-using namespace Actions;
-
-// Actions that don't directly update state.
-// These don't get added to the action/gesture history, since they result in side effects that don't change values in the main state store.
-// These are not saved in a FlowGridAction (.fga) project.
-using ProjectAction = std::variant<
-    Undo, Redo, SetHistoryIndex,
-    OpenProject, OpenEmptyProject, OpenDefaultProject,
-    SaveProject, SaveDefaultProject, SaveCurrentProject, SaveFaustFile, SaveFaustSvgFile>;
-using StateAction = std::variant<
-    OpenFileDialog, CloseFileDialog,
-    ShowOpenProjectDialog, ShowSaveProjectDialog, ShowOpenFaustFileDialog, ShowSaveFaustFileDialog, ShowSaveFaustSvgFileDialog,
-    OpenFaustFile,
-
-    SetValue, SetValues, ToggleValue, ApplyPatch,
-
-    SetImGuiColorStyle, SetImPlotColorStyle, SetFlowGridColorStyle, SetDiagramColorStyle,
-    SetDiagramLayoutStyle,
-
-    CloseApplication>;
-using Action = Combine<ProjectAction, StateAction>::type;
-using ActionID = ID;
-
-// All actions that don't have any member data.
-using EmptyAction = std::variant<
-    Undo,
-    Redo,
-    OpenEmptyProject,
-    OpenDefaultProject,
-    ShowOpenProjectDialog,
-    CloseFileDialog,
-    SaveCurrentProject,
-    SaveDefaultProject,
-    ShowSaveProjectDialog,
-    CloseApplication,
-    ShowOpenFaustFileDialog,
-    ShowSaveFaustFileDialog,
-    ShowSaveFaustSvgFileDialog>;
-
-namespace action {
-
-using ActionMoment = pair<Action, TimePoint>;
-using StateActionMoment = std::pair<StateAction, TimePoint>;
-using Gesture = vector<StateActionMoment>;
-using Gestures = vector<Gesture>;
-
-// Default-construct an action by its variant index (which is also its `ID`).
-// Adapted from: https://stackoverflow.com/a/60567091/780425
-template<ID I = 0> Action Create(ID index) {
-    if constexpr (I >= std::variant_size_v<Action>) throw std::runtime_error{"Action index " + to_string(I + index) + " out of bounds"};
-    else return index == 0 ? Action{std::in_place_index<I>} : Create<I + 1>(index - 1);
-}
-
-#include "../Boost/mp11/mp_find.h"
-
-// E.g. `ActionID action_id = id<action_type>`
-// An action's ID is its index in the `Action` variant.
-// Down the road, this means `Action` would need to be append-only (no order changes) for backwards compatibility.
-// Not worried about that right now, since it should be easy enough to replace with some UUID system later.
-// Index is simplest.
-// Mp11 approach from: https://stackoverflow.com/a/66386518/780425
-template<typename T> constexpr ActionID id = mp_find<Action, T>::value;
-
-#define ActionName(action_var_name) SnakeCaseToSentenceCase(#action_var_name)
-
-// Note: ActionID here is index within `Action` variant, not the `EmptyAction` variant.
-const map<ActionID, string> ShortcutForId = {
-    {id<Undo>, "cmd+z"},
-    {id<Redo>, "shift+cmd+z"},
-    {id<OpenEmptyProject>, "cmd+n"},
-    {id<ShowOpenProjectDialog>, "cmd+o"},
-    {id<OpenDefaultProject>, "shift+cmd+o"},
-    {id<SaveCurrentProject>, "cmd+s"},
-    {id<ShowSaveProjectDialog>, "shift+cmd+s"},
-};
-
-constexpr ActionID GetId(const Action &action) { return action.index(); }
-constexpr ActionID GetId(const StateAction &action) { return action.index(); }
-constexpr ActionID GetId(const ProjectAction &action) { return action.index(); }
-
-string GetName(const ProjectAction &action);
-string GetName(const StateAction &action);
-string GetShortcut(const EmptyAction &);
-string GetMenuLabel(const EmptyAction &);
-Gesture MergeGesture(const Gesture &);
-} // namespace action
-
-using action::ActionMoment;
-using action::Gesture;
-using action::Gestures;
-using action::StateActionMoment;
+using action::ActionMoment, action::Gesture, action::Gestures, action::StateActionMoment;
 
 // todo move to ImVec2ih, or make a new Vec2S16 type
 constexpr U32 PackImVec2ih(const ImVec2ih &unpacked) { return (U32(unpacked.x) << 16) + U32(unpacked.y); }
 constexpr ImVec2ih UnpackImVec2ih(const U32 packed) { return {S16(U32(packed) >> 16), S16(U32(packed) & 0xffff)}; }
 
-// These are needed to fully define equality comparison for `Primitive`.
-constexpr bool operator==(const ImVec2 &lhs, const ImVec2 &rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }
-constexpr bool operator==(const ImVec2ih &lhs, const ImVec2ih &rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }
 constexpr bool operator==(const ImVec4 &lhs, const ImVec4 &rhs) { return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z && lhs.w == rhs.w; }
 
 // E.g. '/foo/bar/baz' => 'baz'

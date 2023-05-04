159945d432f80f4eca39346dd9621531756b0d75
Karl Hiner <karl.hiner@gmail.com>
2023-05-04 14:45:39 -0400

    Hook new text editor up to faust code state. Add file/edit/view menus to new text editor.

--- src/FlowGrid/UI/Editor.cpp
+++ src/FlowGrid/UI/Editor.cpp
@@ -1,28 +1,72 @@
 #include "TextEditor.h"
 
 #include "../App.h"
-#include "../Helper/File.h"
+
+static const Menu FileMenu = {"File", {ShowOpenFaustFileDialog{}, ShowSaveFaustFileDialog{}}};
+
+using namespace ImGui;
 
 void Editor::Render() const {
-    static bool Initialized = false;
-    static const char *file_to_edit = "../src/FlowGrid/UI/Editor.cpp";
-    static auto lang = TextEditor::LanguageDefinition::CPlusPlus();
     static TextEditor editor;
+
+    if (ImGui::BeginMenuBar()) {
+        FileMenu.Draw();
+        if (ImGui::BeginMenu("Edit")) {
+            bool ro = editor.IsReadOnly();
+            if (ImGui::MenuItem("Read-only mode", nullptr, &ro)) editor.SetReadOnly(ro);
+            ImGui::Separator();
+            if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor.CanUndo())) editor.Undo();
+            if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor.CanRedo())) editor.Redo();
+            ImGui::Separator();
+            if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor.HasSelection())) editor.Copy();
+            if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor.HasSelection())) editor.Cut();
+            if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && editor.HasSelection())) editor.Delete();
+            if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr)) editor.Paste();
+            ImGui::Separator();
+            if (ImGui::MenuItem("Select all", nullptr, nullptr)) {
+                editor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(editor.GetTotalLines(), 0));
+            }
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("View")) {
+            if (ImGui::MenuItem("Mariana palette")) editor.SetPalette(TextEditor::GetMarianaPalette());
+            if (ImGui::MenuItem("Dark palette")) editor.SetPalette(TextEditor::GetDarkPalette());
+            if (ImGui::MenuItem("Light palette")) editor.SetPalette(TextEditor::GetLightPalette());
+            if (ImGui::MenuItem("Retro blue palette")) editor.SetPalette(TextEditor::GetRetroBluePalette());
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenuBar();
+    }
+
+    static bool Initialized = false;
+    static auto lang = TextEditor::LanguageDefinition::CPlusPlus();
 
     if (!Initialized) {
         Initialized = true;
         editor.SetLanguageDefinition(lang);
-        editor.SetText(FileIO::read(file_to_edit));
     }
     auto cpos = editor.GetCursorPosition();
 
+    const string editing_file = "no file";
     ImGui::Text(
         "%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, editor.GetTotalLines(),
         editor.IsOverwrite() ? "Ovr" : "Ins",
         editor.CanUndo() ? "*" : " ",
         editor.GetLanguageDefinitionName(),
-        file_to_edit
+        editing_file.c_str()
     );
+
+    ImGui::PushFont(UiContext.Fonts.FixedWidth);
     editor.Render("TextEditor");
-    // auto text = editor.GetText();
+    ImGui::PopFont();
+
+    const auto text = editor.GetText();
+    if (editor.IsTextChanged()) {
+        q(SetValue{s.Faust.Code.Path, text});
+    } else if (s.Faust.Code != text) {
+        // TODO this is not the usual immediate-mode case. Only set text if the text changed.
+        //   Really what I want is for an application undo/redo containing code text changes to do exactly what zep does for undo/redo internally.
+        editor.SetText(s.Faust.Code);
+    }
 }

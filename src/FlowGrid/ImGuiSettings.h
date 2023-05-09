99fe6eea93b33670ad3f4c80f7e56585cf62e6fe
Karl Hiner <karl.hiner@gmail.com>
2023-05-09 14:53:29 -0400

    Move `ImGuiSettings` declarations out of `App.h` (definitions are still in `App.cpp` bc of these damn circular dependencies I'm still figuring out...)

---
+++ src/FlowGrid/ImGuiSettings.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "WindowMember.h"
+
+template<typename T> struct ImChunkStream;
+template<typename T> struct ImVector;
+
+struct ImGuiContext;
+struct ImGuiDockNodeSettings;
+struct ImGuiWindowSettings;
+struct ImGuiTableSettings;
+
+// These Dock/Window/Table settings are `StateMember` duplicates of those in `imgui.cpp`.
+// They are stored here a structs-of-arrays (vs. arrays-of-structs)
+// todo These will show up counter-intuitively in the json state viewers.
+//  Use Raw/Formatted settings in state viewers to:
+//  * convert structs-of-arrays to arrays-of-structs,
+//  * unpack positions/sizes
+Member(
+    DockNodeSettings,
+
+    void Set(const ImVector<ImGuiDockNodeSettings> &, TransientStore &) const;
+    void Apply(ImGuiContext *) const;
+
+    Prop(Vector<ID>, NodeId);
+    Prop(Vector<ID>, ParentNodeId);
+    Prop(Vector<ID>, ParentWindowId);
+    Prop(Vector<ID>, SelectedTabId);
+    Prop(Vector<int>, SplitAxis);
+    Prop(Vector<int>, Depth);
+    Prop(Vector<int>, Flags);
+    Prop(Vector<U32>, Pos); // Packed ImVec2ih
+    Prop(Vector<U32>, Size); // Packed ImVec2ih
+    Prop(Vector<U32>, SizeRef); // Packed ImVec2ih
+);
+
+Member(
+    WindowSettings,
+
+    void Set(ImChunkStream<ImGuiWindowSettings> &, TransientStore &) const;
+    void Apply(ImGuiContext *) const;
+
+    Prop(Vector<ID>, Id);
+    Prop(Vector<ID>, ClassId);
+    Prop(Vector<ID>, ViewportId);
+    Prop(Vector<ID>, DockId);
+    Prop(Vector<int>, DockOrder);
+    Prop(Vector<U32>, Pos); // Packed ImVec2ih
+    Prop(Vector<U32>, Size); // Packed ImVec2ih
+    Prop(Vector<U32>, ViewportPos); // Packed ImVec2ih
+    Prop(Vector<bool>, Collapsed);
+);
+
+Member(
+    TableColumnSettings,
+    // [table_index][column_index]
+    Prop(Vector2D<float>, WidthOrWeight);
+    Prop(Vector2D<ID>, UserID);
+    Prop(Vector2D<int>, Index);
+    Prop(Vector2D<int>, DisplayOrder);
+    Prop(Vector2D<int>, SortOrder);
+    Prop(Vector2D<int>, SortDirection);
+    Prop(Vector2D<bool>, IsEnabled); // "Visible" in ini file
+    Prop(Vector2D<bool>, IsStretch);
+);
+
+Member(
+    TableSettings,
+
+    void Set(ImChunkStream<ImGuiTableSettings> &, TransientStore &store) const;
+    void Apply(ImGuiContext *) const;
+
+    Prop(Vector<ImGuiID>, ID);
+    Prop(Vector<int>, SaveFlags);
+    Prop(Vector<float>, RefScale);
+    Prop(Vector<Count>, ColumnsCount);
+    Prop(Vector<Count>, ColumnsCountMax);
+    Prop(Vector<bool>, WantApply);
+    Prop(TableColumnSettings, Columns);
+);
+
+Member(
+    ImGuiSettings,
+
+    Store Set(ImGuiContext *ctx) const;
+    // Inverse of above constructor. `imgui_context.settings = this`
+    // Should behave just like `ImGui::LoadIniSettingsFromMemory`, but using the structured `...Settings` members
+    //  in this struct instead of the serialized .ini text format.
+    void Apply(ImGuiContext *ctx) const;
+
+    Prop(DockNodeSettings, Nodes);
+    Prop(WindowSettings, Windows);
+    Prop(TableSettings, Tables);
+);

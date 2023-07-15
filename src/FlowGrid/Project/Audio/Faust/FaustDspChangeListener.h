e9604e4d1422b6834c56670739d4350a7331d627
Karl Hiner <karl.hiner@gmail.com>
2023-07-15 14:44:45 -0400

    Update `FaustDspChangeListener` comments.

--- src/FlowGrid/Project/Audio/Faust/FaustDspChangeListener.h
+++ src/FlowGrid/Project/Audio/Faust/FaustDspChangeListener.h
@@ -1,26 +1,27 @@
 #pragma once
 
 /**
-```
-TODO: `Faust` should own FaustDsp and listen to `Faust.Code`, not `Audio`.
-Audio.Faust.Code ->
-    Audio ->
-    FaustDSP ->
-        FaustDspChangeListener ->
-            - Audio.Faust.FaustGraph
-            - Audio.Faust.FaustParams
-            - Audio.Graph.Nodes.Faust
+`FaustDSP` is a wrapper around Faust DSP/Box instances.
+`Audio.Faust` listens to `Audio.Faust.Code` changes and updates `FaustDSP` instances accordingly.
 
-`FaustDsp` is a wrapper around the Faust DSP/Box objects.
-`FaustDsp` listens to `Audio.Faust.Code` changes and creates/rebuilds/destroys the Faust DSP and box instances accordingly.
 Components that listen to `FaustDsp` changes:
 - `Audio.Faust.FaustGraph`: A highly configurable, live-updating block diagram of the Faust DSP.
   - By default, `FaustGraph` matches the FlowGrid style (which is ImGui's dark style).
     But it can be configured to exactly match the Faust SVG diagram style.
     `FaustGraph` can also be rendered as an SVG diagram.
-    When `FaustGraph.Style. hould be the same as the one produced by `faust2svg` with the same DSP code (at least visually!)
+    `FaustGraph.Style` should be the same as the one produced by `faust2svg` with the same DSP code (at least visually!)
 - `Audio.Faust.Params`: Interface for the Faust DSP params. TODO: Not undoable yet.
 - `Audio.Graph.Nodes.Faust`: Updates the MiniAudio node and graph connections to reflect the new DSP.
+
+Here is the chain of notifications/updates in response to a Faust DSP code change:
+```
+Audio.Faust.Code
+    -> Audio.Faust
+        -> Audio.Faust.FaustDSP
+            -> Audio.Faust.FaustGraph
+            -> Audio.Faust.FaustParams
+            -> Audio.Graph.Nodes.Faust
+```
 **/
 
 class dsp;

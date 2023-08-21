2e06d6a1752b7d4034f3979bb32cf16e5a48299b
Karl Hiner <karl.hiner@gmail.com>
2023-08-21 13:19:06 -0400

    DRY up group additions in `FaustParamsUIImpl`.

--- src/FlowGrid/Project/Audio/Faust/FaustParamsUIImpl.h
+++ src/FlowGrid/Project/Audio/Faust/FaustParamsUIImpl.h
@@ -14,35 +14,18 @@ class FaustParamsUIImpl : public UI, public MetaDataUI, public PathBuilder {
 public:
     FaustParamsUIImpl(const FaustParamsUIStyle &style) : Style(style) {}
 
-    void openHorizontalBox(const char *label) override {
-        pushLabel(label);
-        ActiveGroup().Children.emplace_back(Style, Type_HGroup, label);
-        Groups.push(&ActiveGroup().Children.back());
-    }
-    void openVerticalBox(const char *label) override {
-        pushLabel(label);
-        ActiveGroup().Children.emplace_back(Style, Type_VGroup, label);
-        Groups.push(&ActiveGroup().Children.back());
-    }
-    void openTabBox(const char *label) override {
-        pushLabel(label);
-        ActiveGroup().Children.emplace_back(Style, Type_TGroup, label);
-        Groups.push(&ActiveGroup().Children.back());
-    }
+    void openHorizontalBox(const char *label) override { Add(Type_HGroup, label); }
+    void openVerticalBox(const char *label) override { Add(Type_VGroup, label); }
+    void openTabBox(const char *label) override { Add(Type_TGroup, label); }
+
     void closeBox() override {
         Groups.pop();
-        if (popLabel()) {
-            computeShortNames();
-        }
+        if (popLabel()) computeShortNames();
     }
 
     // Active widgets
-    void addButton(const char *label, Real *zone) override {
-        Add(Type_Button, label, zone);
-    }
-    void addCheckButton(const char *label, Real *zone) override {
-        Add(Type_CheckButton, label, zone);
-    }
+    void addButton(const char *label, Real *zone) override { Add(Type_Button, label, zone); }
+    void addCheckButton(const char *label, Real *zone) override { Add(Type_CheckButton, label, zone); }
     void addHorizontalSlider(const char *label, Real *zone, Real init, Real min, Real max, Real step) override {
         addSlider(label, zone, init, min, max, step, false);
     }
@@ -70,31 +53,30 @@ public:
     }
 
     // Passive widgets
-    void addHorizontalBargraph(const char *label, Real *zone, Real min, Real max) override {
-        Add(Type_HBargraph, label, zone, min, max);
-    }
-    void addVerticalBargraph(const char *label, Real *zone, Real min, Real max) override {
-        Add(Type_VBargraph, label, zone, min, max);
-    }
+    void addHorizontalBargraph(const char *label, Real *zone, Real min, Real max) override { Add(Type_HBargraph, label, zone, min, max); }
+    void addVerticalBargraph(const char *label, Real *zone, Real min, Real max) override { Add(Type_VBargraph, label, zone, min, max); }
 
     // Soundfile
     void addSoundfile(const char *, const char *, Soundfile **) override {}
 
     // Metadata declaration
-    void declare(Real *zone, const char *key, const char *value) override {
-        MetaDataUI::declare(zone, key, value);
-    }
+    void declare(Real *zone, const char *key, const char *value) override { MetaDataUI::declare(zone, key, value); }
 
     const FaustParamsUIStyle &Style; // Forwarded to params.
     FaustParam RootParam{Style};
 
 private:
-    void Add(const FaustParamType type, const char *label, Real *zone, Real min = 0, Real max = 0, Real init = 0, Real step = 0, NamesAndValues names_and_values = {}) {
-        ActiveGroup().Children.emplace_back(Style, type, label, zone, min, max, init, step, fTooltip.contains(zone) ? fTooltip.at(zone).c_str() : nullptr, std::move(names_and_values));
-        fFullPaths.push_back(buildPath(label));
+    void Add(const FaustParamType type, const char *label, Real *zone = nullptr, Real min = 0, Real max = 0, Real init = 0, Real step = 0, NamesAndValues names_and_values = {}) {
+        FaustParam &active_group = Groups.empty() ? RootParam : *Groups.top();
+        if (zone == nullptr) { // Group
+            active_group.Children.emplace_back(Style, type, label);
+            Groups.push(&active_group.Children.back());
+            pushLabel(label);
+        } else { // Param
+            active_group.Children.emplace_back(Style, type, label, zone, min, max, init, step, fTooltip.contains(zone) ? fTooltip.at(zone).c_str() : nullptr, std::move(names_and_values));
+            fFullPaths.push_back(buildPath(label));
+        }
     }
-
-    FaustParam &ActiveGroup() { return Groups.empty() ? RootParam : *Groups.top(); }
 
     std::stack<FaustParam *> Groups{};
 };

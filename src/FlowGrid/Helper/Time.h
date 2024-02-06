ebe4531133df92993dec096041cee82f1aaf8ba9
Karl Hiner <karl.hiner@gmail.com>
2024-02-06 16:57:39 -0500

    Add `FormatElapsedMillis` method to `Helper/Time.h`.

--- src/FlowGrid/Helper/Time.h
+++ src/FlowGrid/Helper/Time.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <format>
 
 using namespace std::chrono_literals; // Support literals like `1s` or `500ms`
 
@@ -8,3 +9,8 @@ using namespace std::chrono_literals; // Support literals like `1s` or `500ms`
 using Clock = std::chrono::system_clock; // Main system clock
 using fsec = std::chrono::duration<float>; // float seconds as a std::chrono::duration
 using TimePoint = Clock::time_point;
+
+inline static std::string FormatElapsedMillis(TimePoint start) {
+    auto elapsed = Clock::now() - start;
+    return std::format("{:.3f}ms", std::chrono::duration<float, std::milli>(elapsed).count());
+}

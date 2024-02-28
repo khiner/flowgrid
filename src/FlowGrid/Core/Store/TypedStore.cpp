f774c1f2dea3e7518a9945fe19a0d62f43bd486c
Karl Hiner <karl.hiner@gmail.com>
2024-02-28 17:37:27 -0500

    Use template lambdas and a `Path` helper fn to DRY up `CreatePatch`.

--- src/FlowGrid/Core/Store/TypedStore.cpp
+++ src/FlowGrid/Core/Store/TypedStore.cpp
@@ -7,7 +7,7 @@
 
 #include "immer/algorithm.hpp"
 
-using std::string;
+using std::to_string;
 
 // Naive `diff` method for `immer::vector`s.
 // Callbacks receive an index and a value (for `add` and `remove`) or two values (for `change`).
@@ -18,9 +18,7 @@ void diff(const immer::vector<T> &before, const immer::vector<T> &after, Add add
     while (before_index < before.size() && after_index < after.size()) {
         const auto &before_value = before[before_index];
         const auto &after_value = after[after_index];
-        if (before_value != after_value) {
-            change(before_index, before_value, after_value);
-        }
+        if (before_value != after_value) change(before_index, before_value, after_value);
         before_index++;
         after_index++;
     }
@@ -36,36 +34,33 @@ void diff(const immer::vector<T> &before, const immer::vector<T> &after, Add add
     }
 }
 
-template<typename ValueType>
-void AddOps(const auto &before, const auto &after, const StorePath &base_path, PatchOps &ops) {
-    AddOps(before.template GetMap<ValueType>(), after.template GetMap<ValueType>(), base_path, ops);
-}
+static auto Path(const auto &entry, const StorePath &base) { return entry.first.lexically_relative(base); }
 
 template<typename ValueType>
-void AddOps(const StoreMap<ValueType> &before, const StoreMap<ValueType> &after, const StorePath &base_path, PatchOps &ops) {
+void AddOps(const StoreMap<ValueType> &before, const StoreMap<ValueType> &after, const StorePath &base, PatchOps &ops) {
     diff(
         before,
         after,
-        [&](const auto &added) { ops[added.first.lexically_relative(base_path)] = {PatchOpType::Add, added.second, {}}; },
-        [&](const auto &removed) { ops[removed.first.lexically_relative(base_path)] = {PatchOpType::Remove, {}, removed.second}; },
-        [&](const auto &o, const auto &n) { ops[o.first.lexically_relative(base_path)] = {PatchOpType::Replace, n.second, o.second}; }
+        [&](const auto &added) { ops[Path(added, base)] = {PatchOpType::Add, added.second, {}}; },
+        [&](const auto &removed) { ops[Path(removed, base)] = {PatchOpType::Remove, {}, removed.second}; },
+        [&](const auto &o, const auto &n) { ops[Path(o, base)] = {PatchOpType::Replace, n.second, o.second}; }
     );
 }
 
-void AddOps(const StoreMap<IdPairs> &before, const StoreMap<IdPairs> &after, const StorePath &base_path, PatchOps &ops) {
+void AddOps(const StoreMap<IdPairs> &before, const StoreMap<IdPairs> &after, const StorePath &base, PatchOps &ops) {
     diff(
         before,
         after,
         [&](const auto &added) {
             for (const auto &id_pair : added.second) {
                 const auto serialized = SerializeIdPair(id_pair);
-                ops[added.first.lexically_relative(base_path) / serialized] = {PatchOpType::Add, serialized, {}};
+                ops[Path(added, base) / serialized] = {PatchOpType::Add, serialized, {}};
             }
         },
         [&](const auto &removed) {
             for (const auto &id_pair : removed.second) {
                 const auto serialized = SerializeIdPair(id_pair);
-                ops[removed.first.lexically_relative(base_path) / serialized] = {PatchOpType::Remove, {}, serialized};
+                ops[Path(removed, base) / serialized] = {PatchOpType::Remove, {}, serialized};
             }
         },
         [&](const auto &o, const auto &n) {
@@ -74,11 +69,11 @@ void AddOps(const StoreMap<IdPairs> &before, const StoreMap<IdPairs> &after, con
                 n.second,
                 [&](const auto &added) {
                     const auto serialized = SerializeIdPair(added);
-                    ops[n.first.lexically_relative(base_path) / serialized] = {PatchOpType::Add, serialized, {}};
+                    ops[Path(n, base) / serialized] = {PatchOpType::Add, serialized, {}};
                 },
                 [&](const auto &removed) {
                     const auto serialized = SerializeIdPair(removed);
-                    ops[o.first.lexically_relative(base_path) / serialized] = {PatchOpType::Remove, {}, serialized};
+                    ops[Path(o, base) / serialized] = {PatchOpType::Remove, {}, serialized};
                 },
                 [](const auto &, const auto &) {} // Change callback required but never called for `immer::set`.
             );
@@ -86,44 +81,44 @@ void AddOps(const StoreMap<IdPairs> &before, const StoreMap<IdPairs> &after, con
     );
 }
 
-void AddOps(const StoreMap<immer::set<u32>> &before, const StoreMap<immer::set<u32>> &after, const StorePath &base_path, PatchOps &ops) {
+void AddOps(const StoreMap<immer::set<u32>> &before, const StoreMap<immer::set<u32>> &after, const StorePath &base, PatchOps &ops) {
     diff(
         before,
         after,
         [&](const auto &added) {
             for (auto value : added.second) {
-                ops[added.first.lexically_relative(base_path) / std::to_string(value)] = {PatchOpType::Add, value, {}};
+                ops[Path(added, base) / to_string(value)] = {PatchOpType::Add, value, {}};
             }
         },
         [&](const auto &removed) {
             for (auto value : removed.second) {
-                ops[removed.first.lexically_relative(base_path) / std::to_string(value)] = {PatchOpType::Remove, {}, value};
+                ops[Path(removed, base) / to_string(value)] = {PatchOpType::Remove, {}, value};
             }
         },
         [&](const auto &o, const auto &n) {
             diff(
                 o.second,
                 n.second,
-                [&](auto added) { ops[n.first.lexically_relative(base_path) / std::to_string(added)] = {PatchOpType::Add, added, {}}; },
-                [&](unsigned int removed) { ops[o.first.lexically_relative(base_path) / std::to_string(removed)] = {PatchOpType::Remove, {}, removed}; },
+                [&](auto added) { ops[Path(n, base) / to_string(added)] = {PatchOpType::Add, added, {}}; },
+                [&](unsigned int removed) { ops[Path(o, base) / to_string(removed)] = {PatchOpType::Remove, {}, removed}; },
                 [](const auto &, const auto &) {} // Change callback required but never called for `immer::set`.
             );
         }
     );
 }
 
-void AddOps(const StoreMap<immer::vector<u32>> &before, const StoreMap<immer::vector<u32>> &after, const StorePath &base_path, PatchOps &ops) {
+void AddOps(const StoreMap<immer::vector<u32>> &before, const StoreMap<immer::vector<u32>> &after, const StorePath &base, PatchOps &ops) {
     diff(
         before,
         after,
         [&](const auto &added) {
-            for (uint i = 0; i < added.second.size(); i++) {
-                ops[added.first.lexically_relative(base_path) / std::to_string(i)] = {PatchOpType::Add, added.second[i], {}};
+            for (size_t i = 0; i < added.second.size(); ++i) {
+                ops[Path(added, base) / to_string(i)] = {PatchOpType::Add, added.second[i], {}};
             }
         },
         [&](const auto &removed) {
-            for (uint i = 0; i < removed.second.size(); i++) {
-                ops[removed.first.lexically_relative(base_path) / std::to_string(i)] = {PatchOpType::Remove, {}, removed.second[i]};
+            for (size_t i = 0; i < removed.second.size(); ++i) {
+                ops[Path(removed, base) / to_string(i)] = {PatchOpType::Remove, {}, removed.second[i]};
             }
         },
         [&](const auto &o, const auto &n) {
@@ -132,30 +127,25 @@ void AddOps(const StoreMap<immer::vector<u32>> &before, const StoreMap<immer::ve
                 n.second,
                 // `diff` for `immer::vector<T>` provides callback values of type `pair<std::size_t, const T&>`,
                 // where the first element is the index and the second is the value.
-                [&](std::size_t i, u32 added) { ops[n.first.lexically_relative(base_path) / std::to_string(i)] = {PatchOpType::Add, added, {}}; },
-                [&](std::size_t i, u32 removed) { ops[o.first.lexically_relative(base_path) / std::to_string(i)] = {PatchOpType::Remove, {}, removed}; },
-                [&](std::size_t i, u32 o_el, u32 n_el) {
-                    ops[o.first.lexically_relative(base_path) / std::to_string(i)] = {PatchOpType::Replace, n_el, o_el};
-                }
+                [&](size_t i, u32 added) { ops[Path(n, base) / to_string(i)] = {PatchOpType::Add, added, {}}; },
+                [&](size_t i, u32 removed) { ops[Path(o, base) / to_string(i)] = {PatchOpType::Remove, {}, removed}; },
+                [&](size_t i, u32 o_el, u32 n_el) { ops[Path(o, base) / to_string(i)] = {PatchOpType::Replace, n_el, o_el}; }
             );
         }
     );
 }
 
+template<typename ValueType>
+void AddOps(const auto &before, const auto &after, const StorePath &base, PatchOps &ops) {
+    AddOps(before.template GetMap<ValueType>(), after.template GetMap<ValueType>(), base, ops);
+}
+
 template<typename... ValueTypes>
-Patch TypedStore<ValueTypes...>::CreatePatch(const TypedStore<ValueTypes...> &before, const TypedStore<ValueTypes...> &after, const StorePath &base_path) {
+Patch TypedStore<ValueTypes...>::CreatePatch(const TypedStore<ValueTypes...> &before, const TypedStore<ValueTypes...> &after, const StorePath &base) {
     PatchOps ops{};
-
-    AddOps<bool>(before, after, base_path, ops);
-    AddOps<u32>(before, after, base_path, ops);
-    AddOps<s32>(before, after, base_path, ops);
-    AddOps<float>(before, after, base_path, ops);
-    AddOps<string>(before, after, base_path, ops);
-    AddOps<IdPairs>(before, after, base_path, ops);
-    AddOps<immer::set<u32>>(before, after, base_path, ops);
-    AddOps<immer::vector<u32>>(before, after, base_path, ops);
-
-    return {ops, base_path};
+    // Use template lambda to call `AddOps` for each value type.
+    ([&]<typename T>() { AddOps<T>(before, after, base, ops); }.template operator()<ValueTypes>(), ...);
+    return {ops, base};
 }
 
 // Explicit instantiation for the default value types.
